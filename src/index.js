ac1b6e72f46bbd72c11fb048e0c85f42e2b9b57d
Lima2006 <gabrielchagaslima2015@gmail.com>
2024-11-26 12:21:27 -0300

    feat: simple user login route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 const express = require("express");
-const app = express();
-const PORT = 5000;
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Express.js!" });
+const User = require("./models/user");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((e) => console.error("MongoDB connection error: ", e));
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json({ message: "Success", user });
+      } else {
+        res.json({ message: "Incorrect email or password" });
+      }
+    } else {
+      res.json({ message: "User not found" });
+    }
+  });
 });
 
 app.listen(PORT, () =>
