7796baf1180d620e1a0d573ed06fdd1d0e0e0db9
Lima2006 <gabrielchagaslima2015@gmail.com>
2024-11-28 18:20:06 -0300

    feat: jwt authentication function

---
+++ src/authentication/authenticate-token.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.status(401).json({ message: "Missing token" });
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: "Invalid token" });
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
