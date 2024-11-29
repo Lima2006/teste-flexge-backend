d6187cccbdd6f17ce6ed3695baebe19f7de68ade
Lima2006 <gabrielchagaslima2015@gmail.com>
2024-11-28 23:56:16 -0300

    feat: contract endpoints

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const LoginController = require("./controllers/login-controller");
 const CompanyController = require("./controllers/company-controller");
+const ContractController = require("./controllers/contract-controller");
 const authenticateToken = require("./authentication/authenticate-token");
 
 const routes = Router();
@@ -12,8 +13,14 @@ routes.post("/login", LoginController.login);
 routes.post("/companies", authenticateToken, CompanyController.store);
 routes.get("/companies", authenticateToken, CompanyController.index);
 routes.get("/companies/:id", authenticateToken, CompanyController.show);
-routes.get("/companies/:id", authenticateToken, CompanyController.show);
 routes.put("/companies/:id", authenticateToken, CompanyController.update);
 routes.delete("/companies/:id", authenticateToken, CompanyController.destroy);
+
+// Contract
+routes.post("/contracts", authenticateToken, ContractController.store);
+routes.get("/contracts", authenticateToken, ContractController.index);
+routes.get("/contracts/:id", authenticateToken, ContractController.show);
+routes.put("/contracts/:id", authenticateToken, ContractController.update);
+routes.delete("/contracts/:id", authenticateToken, ContractController.destroy);
 
 module.exports = routes;
